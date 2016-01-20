6b7efbef4586d3c00c49b523362bf0fc841be8bf
Marc Daya <marc@sinistral.net>
2016-01-20 15:55:30 -0700

    Update dev (nREPL) dependencies.
    Bring nREPL dependencies up to date and clean up the POM file.

--- kabob-dev/src/main/clojure/edu/ucdenver/ccp/kabob/dev/cider_repl.clj
+++ kabob-dev/src/main/clojure/edu/ucdenver/ccp/kabob/dev/cider_repl.clj
@@ -1,8 +1,11 @@
+
 (ns edu.ucdenver.ccp.kabob.dev.cider-repl
+  "In which is defined the mechanisms for starting an nREPL server and for
+  manipulating the knowledge base build process."
   (:gen-class)
-  (:require [clojure.tools.nrepl.server :as nrepl-server]
+  (:require [clojure.tools.nrepl.server :refer [start-server]]
             [cider.nrepl :refer (cider-nrepl-handler)]))
 
 (defn -main
   [& args]
-  (nrepl-server/start-server :port 4005 :handler cider-nrepl-handler))
+  (start-server :port 4005 :handler cider-nrepl-handler))
