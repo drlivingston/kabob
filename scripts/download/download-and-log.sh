97031887b5155120e1c10a5ee2379d698e5a7241
bill-baumgartner <bill.baumgartner@gmail.com>
2016-12-06 08:34:33 -0700

    Replaced usage of curl with wget in script used to download ontologies

--- scripts/download/download-and-log.sh
+++ scripts/download/download-and-log.sh
@@ -37,7 +37,7 @@ touch $LOG_FILE
 
 echo "downloading $URL" 
 date | tee -a $LOG_FILE
-curl --remote-name --write-out "file: %{filename_effective} final-url: %{url_effective} size: %{size_download} time: %{time_total} final-time: " -L $URL | tee -a $LOG_FILE
+wget -c -t 0 --timeout 60 --waitretry 10 -P $TARGET_DIR $URL | tee -a $LOG_FILE
 # filename_effective does not get filled on all systems, and parsing the file name from the URL is problematic due to the occasional redirection, e.g. so.owl --> so-xp.obo.owl, so we use ls to get the most recent file downloaded in order to get the downloaded file name
 #ONT_FILE_NAME=$(ls -lhrt $TARGET_DIR | tail -n 1 | tr -s " " | rev | cut -f 1 -d " " | rev)
 #ONT_FILE=$TARGET_DIR$ONT_FILE_NAME
