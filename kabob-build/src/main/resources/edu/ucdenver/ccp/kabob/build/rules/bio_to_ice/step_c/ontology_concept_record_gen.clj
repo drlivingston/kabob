d48e9a91a66b27d1513802e11cab26917d5f28fc
callahantiff <callahantiff@gmail.com>
2017-04-26 12:27:42 -0600

    Commented out definition field value

--- kabob-build/src/main/resources/edu/ucdenver/ccp/kabob/build/rules/bio_to_ice/step_c/ontology_concept_record_gen.clj
+++ kabob-build/src/main/resources/edu/ucdenver/ccp/kabob/build/rules/bio_to_ice/step_c/ontology_concept_record_gen.clj
@@ -8,9 +8,9 @@
          (?/id_field_value rdf/type ccp/IAO_EXT_0000194) ; id field value
          (?/id_field_value rdf/type ?/id) ; ontology concept identifier
          (?/id_field_value rdfs/label ?/id_string)
-         (?/record obo/BFO_0000051 ?/definition_field_value) ; has part
-         (?/definition_field_value rdf/type ccp/IAO_EXT_0000195) ; ontology concept record definition field value
-         (?/definition_field_value rdfs/label ?/definition)
+         ;(?/record obo/BFO_0000051 ?/definition_field_value) ; has part
+         ;(?/definition_field_value rdf/type ccp/IAO_EXT_0000195) ; ontology concept record definition field value
+         ;(?/definition_field_value rdfs/label ?/definition)
          (?/record obo/BFO_0000051 ?/label_field_value)
          (?/label_field_value rdf/type ccp/IAO_EXT_0000196) ; ontology concept record label field value
          (?/label_field_value rdfs/label ?/label))
@@ -18,8 +18,8 @@
                     :ns "ccp" :prefix "R_" :suffix ""}]
            [?/id_field_value {:ln (:sha-1 ?/id)
                :ns "ccp" :prefix "F_" :suffix ""}]
-           [?/definition_field_value {:ln (:sha-1 ?/definition)
-               :ns "ccp" :prefix "F_" :suffix ""}]
+           ;[?/definition_field_value {:ln (:sha-1 ?/definition)
+               ;:ns "ccp" :prefix "F_" :suffix ""}]
            [?/label_field_value {:ln (:sha-1 ?/label)
                :ns "ccp" :prefix "F_" :suffix ""}])
   :sparql-string "prefix ccp: <http://ccp.ucdenver.edu/obo/ext/>
