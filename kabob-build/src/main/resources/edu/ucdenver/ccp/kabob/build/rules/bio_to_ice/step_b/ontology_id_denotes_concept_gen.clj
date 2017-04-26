25412a6ed8f71ad4bde0794c27ed9fa936ed15e7
callahantiff <callahantiff@gmail.com>
2017-04-26 11:33:14 -0600

    Created the ontology id denotes concept gen rule

---
+++ kabob-build/src/main/resources/edu/ucdenver/ccp/kabob/build/rules/bio_to_ice/step_b/ontology_id_denotes_concept_gen.clj
@@ -0,0 +1,15 @@
+;; ------------------------------------------------------------------------
+;; --------- Ontology Concept Identifier Denotes Ontology Concept ---------
+;; ------------------------------------------------------------------------
+;; create id denotes concept triple for every ontology concept
+`{:name "ontology-id-denotes-concept-gen"
+  :head ((?/id obo/IAO_0000219 ?/sc)) ; denotes
+  :sparql-string "prefix ccp: <http://ccp.ucdenver.edu/obo/ext/>
+                  prefix obo: <http://purl.obolibrary.org/obo/>
+                  select ?sc {
+                  ?root_id rdf:type ccp:IAO_EXT_0000190 .
+                  ?root_id obo:IAO_0000219 ?root_class .
+                  ?sc rdfs:subClassOf* ?root_class .
+                  }"
+  :reify ([?/id {:ln (:localname ?/sc)
+                 :ns "ccp" :prefix "" :suffix ""}])}
