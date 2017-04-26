9c7159c1daa81e64c9a94b220119752c5f2af708
callahantiff <callahantiff@gmail.com>
2017-04-26 11:43:54 -0600

    Added a triple to type the id

--- kabob-build/src/main/resources/edu/ucdenver/ccp/kabob/build/rules/bio_to_ice/step_b/ontology_id_denotes_concept_gen.clj
+++ kabob-build/src/main/resources/edu/ucdenver/ccp/kabob/build/rules/bio_to_ice/step_b/ontology_id_denotes_concept_gen.clj
@@ -3,7 +3,8 @@
 ;; ------------------------------------------------------------------------
 ;; create id denotes concept triple for every ontology concept
 `{:name "ontology-id-denotes-concept-gen"
-  :head ((?/id obo/IAO_0000219 ?/sc)) ; denotes
+  :head ((?/id obo/IAO_0000219 ?/sc) ; denotes
+         (?/id rdf/type ccp/IAO_EXT_0000088)) ; ontology concept identifier
   :sparql-string "prefix ccp: <http://ccp.ucdenver.edu/obo/ext/>
                   prefix obo: <http://purl.obolibrary.org/obo/>
                   select ?sc {
