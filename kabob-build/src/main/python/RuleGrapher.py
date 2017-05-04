2d1fbe1fa324b59a118d4ee691f6db3700c3d04b
callahantiff <callahantiff@gmail.com>
2017-05-04 01:02:42 -0600

    Updated rules to account for rules using sprawl-string instead of body
    Script is in transition and will be updated as more rules are converted
    to use sparql-string.

--- kabob-build/src/main/python/RuleGrapher.py
+++ kabob-build/src/main/python/RuleGrapher.py
@@ -46,7 +46,6 @@ def SpecialChar(trip_list):
     return mod_trip
 
 
-
 def TripleMaker(rule):
     '''
     function takes a list of of text, composed of parts of triples, from a Clojure rule and performs some string
@@ -67,14 +66,81 @@ def TripleMaker(rule):
         if '/' in rlist and ';' not in rlist:
             triples.append([node.replace('/', '') if node.startswith('?') or node.startswith('_')
                             else node.replace('/', ':') if not node.startswith('[') and not node.startswith('_')
-                            else node.replace('/', ':') if '[' and '/' in node
-                            else node for node in ''.join(rlist).split(' ')])
+            else node.replace('/', ':') if '[' and '/' in node
+            else node for node in ''.join(rlist).split(' ')])
 
-    triples = [' '.join(i).split() for i in triples] #remove form nested lists whitespace
-    modified_triples = SpecialChar(triples) #deal with special characters
+    triples = [' '.join(i).split() for i in triples]  # remove form nested lists whitespace
+    modified_triples = SpecialChar(triples)  # deal with special characters
 
     return modified_triples
 
+
+def TripleMaker_SS(rule):
+    '''
+    function takes a list of of text, composed of parts of triples, from a Clojure rule and performs some string
+    formatting (e.g., ?/ice --> ?ice, rdfs/label --> rdfs:label). The function returns a list of lists where each list
+    consists of a triple (SPARQL-STRING Version)
+    :param rule: list of text
+    :return: list of lists where each list is a triple
+    '''
+
+    if 'select' in rule:
+        mod_triples = []
+        if 'BIND' in rule:
+            triple = [y for y in
+                      filter(None, [x.strip('"') for x in rule.split('BIND')[:-1][0].split(',') if 'select' not in x])
+                      if '?' in y]
+        else:
+            triple = [y for y in filter(None, [x.strip('"') for x in rule.split(',') if 'select' not in x]) if '?' in y]
+        for i in triple:
+            if 'minus' in i:
+                trip = i.split('{')[-1].rstrip().strip('}').split(' ')
+                trip[0] = '-' + str(trip[0])
+                mod_triples.append(trip[:3])
+            elif 'optional' in i:
+                trip = i.split('{')[-1].rstrip().strip('}').split(' ')
+                trip[0] = 'o' + str(trip[0])
+                # trip[0] = str(trip[0])
+                mod_triples.append(trip[:3])
+            else:
+                mod_triples.append([x.strip('}').strip("'") for x in filter(None, i.strip('.').split(' '))])
+
+        return mod_triples
+
+    if '(' in rule:
+        r_set = []
+        for i in rule.split(')'):
+            if '/' in i and '@' not in i:
+                r_set.append(re.sub(',', ' ', i.strip().split('(')[-1]))
+        # split triples into separate lists and clean up formatting
+        triples = []
+        for rlist in r_set:
+            if '/' in rlist and ';' not in rlist:
+                triples.append([node.replace('/', '') if
+                                node.startswith('?') or
+                                node.startswith('_')
+                                else node.replace('/', ':') if not node.startswith('[') and not node.startswith('_')
+                else node.replace('/', ':') if '[' and '/' in node
+                else node for node in ''.join(rlist).split(' ')])
+
+        triples = [x for x in [' '.join(i).split() for i in triples] if
+                   not any([y.startswith(':') for y in x]) and not len(x) > 3]  # remove form nested lists whitespace
+        mod_triples2 = SpecialChar(triples)  # deal with special characters
+
+        return mod_triples2
+
+
+def RuleSplitter(rule, start, stop):
+    '''
+    function takes a string as well as two sub-strings and returns the string between the sub-strings
+    :param rule: string representing a rule
+    :param start: sub-string that is present in rule
+    :param stop: sub-string that is present in rule
+    :return: sub-string from rule that was between the start and stop sub-strings
+    '''
+
+    result = re.search(str(start) + '(.*)' + str(stop), rule)
+    return result.group(1)
 
 
 def RulesDict(rule_set):
@@ -92,14 +158,38 @@ def RulesDict(rule_set):
 
     for rule in rule_set:
         if [x for x in rule.split(':') if x.startswith('name')]:
-            key = [i for i in [x.strip('name').replace('"', '').strip() for x in rule.split(':') if x.startswith('name')] if x][0]
+            key = [i for i in [x.strip('name').replace('"', '').strip() for x in rule.split(':') if x.startswith('name')] if x][0].strip(',')
             rules_dict[key] = {}
-            rules_dict[key]['head'] = []; rules_dict[key]['body'] = []
+            rules_dict[key]['head'] = [];
+            rules_dict[key]['body'] = []
             rules_dict[key]['head'] += TripleMaker([x.strip('head') for x in rule.split(':') if x.startswith('head')])
             rules_dict[key]['body'] += TripleMaker([x.strip('body') for x in rule.split(':') if x.startswith('body')])
 
     return rules_dict
 
+
+def RulesDict_SS(rule_set):
+    '''
+    function takes a list of rule lists as inputs, processes the rules in an effort to isolate the specific parts that
+    are needed for graphical representation (e.g., :name, :head, and :body). The function returns a dictionary
+    where the keys are the names of each rule (extracted from the 'name' field of the rule) and the values are a list
+    of triple lists (SPARQL-STRING Version)
+    :param rule_set: list of rules lists
+    :return: dictionary
+    where the keys are the names of each rule (extracted from the 'name' field of the rule) and the values are a list of
+    triple lists
+    '''
+    rules_dict = {}
+
+    for rule in rule_set:
+        if 'name' in rule:
+            key = re.sub('[", ]', '', RuleSplitter(rule, ':name', ':')).split(':')[0]
+            rules_dict[key] = {}
+            rules_dict[key]['head'] = []; rules_dict[key]['sparql-string'] = []
+            rules_dict[key]['head'] += TripleMaker_SS(RuleSplitter(rule, ':head', ':sparql-string').split(':reify')[0].strip())
+            rules_dict[key]['sparql-string'] += [x for x in TripleMaker_SS(RuleSplitter(rule, ':sparql-string', '"')) if ':reify' not in x]
+
+    return rules_dict
 
 
 def GraphMaker(triple_dict):
@@ -134,35 +224,86 @@ def GraphMaker(triple_dict):
             else:
                 graph.add_edge(str(trip[0]), str(trip[2]), label=str(trip[1]), color='orange', arrowhead='normal', arrowsize=1.0)
 
+        if 'body' in value.keys():
+            for trip in value['body']: #body of rule - set node and edge attributes
+                if str(trip[0]).startswith('?') or str(trip[0]).startswith('['):
+                    graph.add_node(str(trip[0]),shape='box', label=str(trip[0]), style='filled', color='gray65')
+                else:
+                    graph.add_node(str(trip[0]), label=str(trip[0]), shape='ellipse', color='gray30')
+                if str(trip[2]).startswith('?') or str(trip[2]).startswith('['):
+                    graph.add_node(str(trip[2]),  shape='box', label=str(trip[2]), style='filled', color='gray65')
+                else:
+                    graph.add_node(str(trip[2]), label=str(trip[2]), shape='ellipse', color='gray30')
 
-        for trip in value['body']: #body of rule - set node and edge attributes
-            if str(trip[0]).startswith('?') or str(trip[0]).startswith('['):
-                graph.add_node(str(trip[0]),shape='box', label=str(trip[0]), style='filled', color='gray65')
-            else:
-                graph.add_node(str(trip[0]), label=str(trip[0]), shape='ellipse', color='gray30')
-            if str(trip[2]).startswith('?') or str(trip[2]).startswith('['):
-                graph.add_node(str(trip[2]),  shape='box', label=str(trip[2]), style='filled', color='gray65')
-            else:
-                graph.add_node(str(trip[2]), label=str(trip[2]), shape='ellipse', color='gray30')
+                # set edges and edge attributes
+                if 'subClass' in str(trip[1]): #gives subClassOf relations a different a different arrowhead type
+                    graph.add_edge(str(trip[0]), str(trip[2]), label=str(trip[1]), color='gray65', arrowhead='onormal', arrowsize=1.5)
+                elif '!=' in str(trip[1]):
+                    graph.add_edge(str(trip[0]), str(trip[2]), label=str(trip[1]), color='red', arrowhead='tee', arrowsize=1.0)
+                else:
+                    graph.add_edge(str(trip[0]), str(trip[2]), label=str(trip[1]), color='gray65', arrowhead='normal', arrowsize=1.0)
 
-            # set edges and edge attributes
-            if 'subClass' in str(trip[1]): #gives subClassOf relations a different a different arrowhead type
-                graph.add_edge(str(trip[0]), str(trip[2]), label=str(trip[1]), color='gray65', arrowhead='onormal', arrowsize=1.5)
-            elif '!=' in str(trip[1]):
-                graph.add_edge(str(trip[0]), str(trip[2]), label=str(trip[1]), color='red', arrowhead='tee', arrowsize=1.0)
-            else:
-                graph.add_edge(str(trip[0]), str(trip[2]), label=str(trip[1]), color='gray65', arrowhead='normal', arrowsize=1.0)
+        if 'sparql-string' in value.keys():
+            for trip in value['sparql-string']: #body of rule - set node and edge attributes
+                if str(trip[0]).startswith('?'):
+                    graph.add_node(str(trip[0]),shape='box', label=str(trip[0]), style='filled', color='gray65')
+                if not str(trip[0]).startswith('?') and not str(trip[0].strip('-')).startswith('?'):
+                    graph.add_node(str(trip[0]), label=str(trip[0]), shape='ellipse', color='gray30')
+                if str(trip[0]).startswith('-'):
+                    if str(trip[0].strip('-')).startswith('?'):
+                        graph.add_node(str(trip[0].strip('-')), shape='box', style='filled', label=str(trip[0].strip('-')), color='red')
+                    if not str(trip[0].strip('-')).startswith('?'):
+                        graph.add_node(str(trip[0].strip('-')), shape='ellipse', label=str(trip[0].strip('-')), color='red')
+                if str(trip[0]).startswith('o'):
+                    if str(trip[0].strip('o')).startswith('?'):
+                        graph.add_node(str(trip[0].strip('o')), shape='box', style='filled', label=str(trip[0].strip('o')), color='green')
+                    if not str(trip[0].strip('o')).startswith('?'):
+                        graph.add_node(str(trip[0].strip('o')), shape='ellipse', label=str(trip[0].strip('-')), color='green')
+
+                # set edges and edge attributes
+                if ('subClass' in str(trip[1]) and not (str(trip[0]).startswith('-') or str(trip[0]).startswith('o'))): #gives subClassOf relations a different a different arrowhead type
+                    graph.add_edge(str(trip[0]), str(trip[2]), label=str(trip[1]), color='gray65', arrowhead='onormal', arrowsize=1.5)
+
+                if ('subClass' in str(trip[1]) and str(trip[0]).startswith('-')):
+                    graph.add_edge(str(trip[0].strip('-')), str(trip[2]), label=str(trip[1]), color='red', arrowhead='onormal', arrowsize=1.5)
+                    if str(trip[2]).startswith('?'):
+                        graph.add_node(str(trip[2]), shape='box', style='filled', label=str(trip[2]), color='red')
+                    if not str(trip[2]).startswith('?'):
+                        graph.add_node(str(trip[2]), label=str(trip[2]), shape='ellipse', color='red')
+
+                if ('subClass' not in str(trip[1]) and str(trip[0]).startswith('-')):
+                    graph.add_edge(str(trip[0].strip('-')), str(trip[2]), label=str(trip[1]), color='red', arrowhead='normal', arrowsize=1.0)
+                    if str(trip[2]).startswith('?'):
+                        graph.add_node(str(trip[2]), shape='box', style='filled', label=str(trip[2]), color='red')
+                    if not str(trip[2]).startswith('?'):
+                        graph.add_node(str(trip[2]), label=str(trip[2]), shape='ellipse', color='red')
+
+                if ('subClass' in str(trip[1]) and str(trip[0]).startswith('o')):
+                    graph.add_edge(str(trip[0].strip('o')), str(trip[2]), label=str(trip[1]), color='green', arrowhead='onormal', arrowsize=1.5)
+                    if str(trip[2]).startswith('?'):
+                        graph.add_node(str(trip[2]), shape='box', style='filled', label=str(trip[2]), color='green')
+                    if not str(trip[2]).startswith('?'):
+                        graph.add_node(str(trip[2]), label=str(trip[2]), shape='ellipse', color='green')
+
+                if ('subClass' not in str(trip[1]) and str(trip[0]).startswith('o')):
+                    graph.add_edge(str(trip[0].strip('o')), str(trip[2]), label=str(trip[1]), color='green', arrowhead='normal', arrowsize=1.0)
+                    if str(trip[2]).startswith('?'):
+                        graph.add_node(str(trip[2]), shape='box', style='filled', label=str(trip[2]), color='green')
+                    if not str(trip[2]).startswith('?'):
+                        graph.add_node(str(trip[2]), label=str(trip[2]), shape='ellipse', color='green')
+
+                if ('subClass' not in str(trip[1]) and not (str(trip[0]).startswith('-')) or not str(trip[0].startswith('o'))):
+                    graph.add_edge(str(trip[0]), str(trip[2]), label=str(trip[1]), color='gray65', arrowhead='normal', arrowsize=1.0)
 
         graph_dict[key] = graph
 
     return graph_dict
 
 
-
 def GraphViewer(graph_dict, output):
     '''
     function takes a dictionary, keyed by rule name, where the keys are a Networkx graph generator as input and returns
-    a graphical representation of the rule. The file is named after the invidual rules and output to the location of
+    a graphical representation of the rule. The file is named after the individual rules and output to the location of
     the Clojure rule file
     :param graph_dict: dictionary, keyed by rule name, where the keys are a Networkx graph generator
     :param output: a string containing the file pathway information for outputting the graphical representations
@@ -192,22 +333,30 @@ def main():
     args = parser.parse_args()
 
     # iterate over all files in a directory
-    # root = "/Users/tiffanycallahan/Dropbox/GraduateSchool/PhD/LabWork/KaBOB/KaBOB_Dev/kabob/kabob-build/src/main/resources/edu/ucdenver/ccp/kabob/build/rules/entity_typing"
+    # root = "/Users/tiffanycallahan/Dropbox/GraduateSchool/PhD/LabWork/KaBOB/KaBOB_Dev/kabob/kabob-build/src/main/resources/edu/ucdenver/ccp/kabob/build/rules/bio_to_ice/step_a"
     # files = os.listdir(root)
     for root, dirs, files in os.walk(args.input):
         for file in files:
             if file.endswith(".clj"):
+                print file
                 rule_file = (os.path.join(root, file))
                 rules = []
                 for line in open(rule_file).readlines():
                     line = re.sub(r'\s*;.*', '', line)
                     rules.append(line.strip())
+                if any([x for x in rules if ':sparql-string' in x]): # sparql-string
+                    rules_mod = [x for x in [x.strip() for x in re.split('`', ','.join(rules))] if x] #split data into list of rules
+                    rules_mod = filter(None, [rule for rule in rules_mod if '@' not in rule]) #not making figures for rules with macros - REMOVE THIS ONCE RULES UPDATED1
+                    rules_dict = RulesDict_SS(rules_mod) #create dictionary to store triples by rule
+                    graphs = GraphMaker(rules_dict) #create a graph for each rule - saves to a dictionary
+                    GraphViewer(graphs, root) #create a visualization for each rule graph
 
-                rules_mod = [x for x in [x.strip() for x in re.split('{', ','.join(rules))] if x] #split data into list of rules
-                rules_mod = [rule for rule in rules_mod if '@' not in rule] #not making figures for rules with macros - REMOVE THIS ONCE RULES UPDATED
-                rules_dict = RulesDict(rules_mod) #create dictionary to store triples by rule
-                graphs = GraphMaker(rules_dict) #create a graph for each rule - saves to a dictionary
-                GraphViewer(graphs, root) #create a visualization for each rule graph
+                if any([x for x in rules if ':body' in x]): # body
+                    rules_mod = [x for x in [x.strip() for x in re.split('{', ','.join(rules))] if x]  # split data into list of rules
+                    rules_mod = [rule for rule in rules_mod if '@' not in rule]  # not making figures for rules with macros - REMOVE THIS ONCE RULES UPDATED1
+                    rules_dict = RulesDict(rules_mod) #create dictionary to store triples by rule
+                    graphs = GraphMaker(rules_dict) #create a graph for each rule - saves to a dictionary
+                    GraphViewer(graphs, root) #create a visualization for each rule graph
 
                 print 'Created Graphical Representation of Rule: ' + str(rule_file)
 
