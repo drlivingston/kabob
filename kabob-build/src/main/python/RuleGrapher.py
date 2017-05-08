bbf6e66da77b0f51f4452c1312768e70ed03a05c
callahantiff <callahantiff@gmail.com>
2017-05-08 09:17:54 -0600

    Replaced 'Literal' with 'Constant' in figure legends

--- kabob-build/src/main/python/RuleGrapher.py
+++ kabob-build/src/main/python/RuleGrapher.py
@@ -309,18 +309,18 @@ def GraphMaker(triple_dict):
                             graph.add_node(str(trip[2]), label=str(trip[2]), shape='ellipse', color='gray65')
 
             #add legend
-            graph.add_node('bodyLiteral', shape='ellipse', label='bodyLiteral', color='gray65')
+            graph.add_node('bodyConstant', shape='ellipse', label='bodyConstant', color='gray65')
             # head + not subclass
             graph.add_node('?headVar', shape='box', style='filled', label='?headVar', color='orange')
-            graph.add_edge('?headVar', 'bodyLiteral', label='subClassOf', color='orange', arrowhead='onormal', arrowsize=1.5)
+            graph.add_edge('?headVar', 'bodyConstant', label='subClassOf', color='orange', arrowhead='onormal', arrowsize=1.5)
             # minus + not subclass
             graph.add_node('?var{MINUS}', shape='box', style='filled', label='?var{MINUS}', color='red')
-            graph.add_edge('?var{MINUS}', 'bodyLiteral', label='!subClassOf', color='red', arrowhead='normal', arrowsize=1.0)
+            graph.add_edge('?var{MINUS}', 'bodyConstant', label='!subClassOf', color='red', arrowhead='normal', arrowsize=1.0)
             # option
-            graph.add_edge('bodyLiteral', 'literal', label='!not', color='red', arrowhead='tee', arrowsize=1.0)
+            graph.add_node('Constant', shape='ellipse', label='Constant', color='green')
+            graph.add_edge('bodyConstant', 'Constant', label='!equal', color='red', arrowhead='tee', arrowsize=1.0)
             graph.add_node('?var{OPTION}', shape='box', style='filled', label='?var{OPTION}', color='green')
-            graph.add_node('literal', shape='ellipse', label='literal', color='green')
-            graph.add_edge('?var{OPTION}', 'literal', label='subClassOf', color='green', arrowhead='onormal', arrowsize=1.5)
+            graph.add_edge('?var{OPTION}', 'Constant', label='subClassOf', color='green', arrowhead='onormal', arrowsize=1.5)
 
         graph_dict[key] = graph
 
