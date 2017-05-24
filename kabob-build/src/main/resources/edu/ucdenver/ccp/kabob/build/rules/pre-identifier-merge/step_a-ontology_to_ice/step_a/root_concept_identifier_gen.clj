5fa5f5aade227bdc5670e92e5462feeafdb3c62b
bill-baumgartner <bill.baumgartner@gmail.com>
2017-05-24 15:28:09 -0600

    Added rule description for root_concept_identifier_gen

--- kabob-build/src/main/resources/edu/ucdenver/ccp/kabob/build/rules/pre-identifier-merge/step_a-ontology_to_ice/step_a/root_concept_identifier_gen.clj
+++ kabob-build/src/main/resources/edu/ucdenver/ccp/kabob/build/rules/pre-identifier-merge/step_a-ontology_to_ice/step_a/root_concept_identifier_gen.clj
@@ -1,10 +1,10 @@
 ;; -------------------------------------------
 ;; --------- Root Concept Assignment ---------
 ;; -------------------------------------------
-;; create ICE URI's for each term in the ontology
 `{:name "root-concept-identifier-gen"
-  :head ((?/id rdf/type ccp/IAO_EXT_0000190) ; ontology root concept identifer
-         (?/id obo/IAO_0000219 ?/root)) ; denotes
+  :description "This rule creates an identifier for each root ontology class and types it as a root concept identifier (IAO_EXT_0000190)"
+  :head ((?/id rdf/type ccp/IAO_EXT_0000190) ;; ccp:ontology root concept identifer
+         (?/id obo/IAO_0000219 ?/root)) ;; obo:denotes
   :reify ([?/id {:ln (:localname ?/root)
                  :ns "ccp" :prefix "" :suffix ""}])
   :sparql-string "select ?root {
