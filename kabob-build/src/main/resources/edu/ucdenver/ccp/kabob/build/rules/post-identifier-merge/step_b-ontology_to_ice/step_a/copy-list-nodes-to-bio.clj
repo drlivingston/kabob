a40ddbad3634261d68bccf3b77b5fc5f104d4163
bill-baumgartner <bill.baumgartner@gmail.com>
2017-05-24 23:34:13 -0600

    Initial commit of a rule to transfer rdf:List nodes to bio world via the RDF list record

---
+++ kabob-build/src/main/resources/edu/ucdenver/ccp/kabob/build/rules/post-identifier-merge/step_b-ontology_to_ice/step_a/copy-list-nodes-to-bio.clj
@@ -0,0 +1,15 @@
+`{:name "copy-list-nodes-to-bio"
+  :description "This rule makes a copy of all rdf:List instances and connects the copy to a generated List Record (ccp:IAO_EXT_0000317) instance."
+  :head ((?/list_record obo/IAO_0000219 ?/listmember) ;; obo:denotes
+        (?/list_record rdf/type obo/IAO_EXT_0000317) ;; ccp:RDF list record
+        (?/list_record obo/IAO_0000219 ?/bio_listmember) ;; obo:denotes
+        (?/bio_listmember rdf/Type rdf/List))
+  :reify ([?/list_record {:ln (:sha-1 ?/listmember)
+                     :ns "ccp" :prefix "R_"}]
+          [?/bio_listmember {:ln (:sha-1 ?/listmember)
+                     :ns "ccp" :prefix "L_"}])
+  :sparql-string "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+                  select distinct ?listmember {
+                    ?listmember rdf:type rdf:List .
+                  }"
+  }
