fdd3de491544b012118d4a01a724a1f29f0aadf1
bill-baumgartner <bill.baumgartner@gmail.com>
2017-05-31 12:25:06 -0600

    Add transfer-rdf-list-links-with-nil to connect rdf:List members to nil where appropriate

--- resources/rules/post_identifier_merge/step_b_ontology_to_ice/step_b/link_bio_with_nil.clj
+++ resources/rules/post_identifier_merge/step_b_ontology_to_ice/step_b/link_bio_with_nil.clj
@@ -1,8 +1,11 @@
-`{:name "link-bio-with-nil"
-  :description "This link-bio-with-properties rule requires both
-  things being linked to be 'bioentities' and thus this rule excludes
-  links within RDF lists that connect to rdf:nil. This rule fills in
-  those missing links."
-  :head ((?/bio ?/p rdf/nil))
-  :body ((?/c obo/IAO_0000219 ?/bio)
-         (?/c ?/p rdf/nil))}
+`{:name "transfer-rdf-list-links-with-nil"
+  :description "This rule transfers links to rdf:nil from list members to bio world. These links are
+  predominantly '?x rdf:rest rdf/nil'."
+  :head ((?/bio_list_member ?/p rdf/nil))
+  :sparql-string "select ?bio_list_member ?p {
+  ?list_record rdf:type ccp:IAO_EXT_0000317 .
+  ?list_record obo:IAO_0000219 ?list_member .
+  ?list_member ?p rdf:nil .
+  ?list_record obo:IAO_0000219 ?bio_list_member .
+  filter (?list_member != ?bio_list_member)
+  }"}
