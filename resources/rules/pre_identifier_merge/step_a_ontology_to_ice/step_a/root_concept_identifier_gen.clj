0e4cbee6bfd666e9ec42b05ab8a0c91b109f915b
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-06 15:16:55 -0600

    Revised root_concept_identifier_gen rule to account for subClasses of owl:Thing

--- resources/rules/pre_identifier_merge/step_a_ontology_to_ice/step_a/root_concept_identifier_gen.clj
+++ resources/rules/pre_identifier_merge/step_a_ontology_to_ice/step_a/root_concept_identifier_gen.clj
@@ -8,9 +8,14 @@
   :reify ([?/id {:ln (:localname ?/root)
                  :ns "ccp" :prefix "" :suffix ""}])
   :sparql-string "select ?root {
-		  ?root rdf:type owl:Class .
-		  minus{?root rdfs:subClassOf ?class}
-		  minus{?root owl:deprecated true} 
-		  filter(!contains(str(?root), '/bnode/')) 
-		}"
+  ?root rdf:type owl:Class .
+  # return classes that are subClassOf owl:Thing or classes that are not subClassOf anything
+  filter ((exists {?root rdfs:subClassOf owl:Thing}) || (not exists {?root rdfs:subClassOf ?class}))
+  # exclude all deprecated classes
+  filter (not exists {?root owl:deprecated true})
+  # exclude any blank nodes
+  filter (!contains(str(?root), '/bnode/'))
+  # exclude the oboInOwl:ObsoleteClass class
+  minus {?root rdfs:label 'Obsolete Class'}
+  }"
 }
