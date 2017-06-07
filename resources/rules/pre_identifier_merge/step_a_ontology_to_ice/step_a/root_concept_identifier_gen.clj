f0889a31f876c6e7f959dc72d397105d04570e04
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-06 19:01:53 -0600

    revised root_concept_identifier_gen rule
    Added handling for the case where a class is both a subclass of another
    class and an explicit subclass of owl:Thing
    
    Also excluded rdf and skos classes

--- resources/rules/pre_identifier_merge/step_a_ontology_to_ice/step_a/root_concept_identifier_gen.clj
+++ resources/rules/pre_identifier_merge/step_a_ontology_to_ice/step_a/root_concept_identifier_gen.clj
@@ -9,18 +9,24 @@
                  :ns "ccp" :prefix "" :suffix ""}])
   :sparql-string "prefix oboInOwl: <http://www.geneontology.org/formats/oboInOwl#>
   select ?root {
-  ?root rdf:type owl:Class .
-  # return classes that are subClassOf owl:Thing or classes that are not subClassOf anything
-  filter ((exists {?root rdfs:subClassOf owl:Thing}) || (not exists {?root rdfs:subClassOf ?class}))
-  # exclude all deprecated classes
-  filter (not exists {?root owl:deprecated true})
-  # exclude any blank nodes
-  filter (!contains(str(?root), '/bnode/'))
-  # exclude the oboInOwl:ObsoleteClass class
-  filter (?root != oboInOwl:ObsoleteClass)
-  # exclude ICE world concepts (IAO, CCP extension ontology, OA ontology)
-  filter (!contains (str(?root), 'ext/IAO_'))
-  filter (!contains (str(?root), 'obo/IAO_'))
-  filter (!contains (str(?root), 'http://www.w3.org/ns/oa#'))
-  }"
+                ?root rdf:type owl:Class .
+                # return classes that are subClassOf owl:Thing or classes that are not subClassOf anything
+                filter ((exists {?root rdfs:subClassOf owl:Thing}) || (not exists {?root rdfs:subClassOf ?class}))
+                # but don't return classes that are subClassOf owl:Thing and subClass of some other class
+                filter (not exists {?root rdfs:subClassOf owl:Thing .
+                                    ?root rdfs:subClassOf ?class .
+                                    filter (?class != owl:Thing)})
+                # exclude all deprecated classes
+                filter (not exists {?root owl:deprecated true})
+                # exclude any blank nodes
+                filter (!contains(str(?root), '/bnode/'))
+                # exclude the oboInOwl:ObsoleteClass class
+                filter (?root != oboInOwl:ObsoleteClass)
+                # exclude ICE world concepts (IAO, CCP extension ontology, OA ontology)
+                filter (!contains (str(?root), 'ext/IAO_'))
+                filter (!contains (str(?root), 'obo/IAO_'))
+                filter (!contains (str(?root), 'http://www.w3.org/ns/oa#'))
+                filter (!contains (str(?root), 'http://www.w3.org/2004/02/skos/core#'))
+                filter (!contains (str(?root), 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'))
+}"
 }
