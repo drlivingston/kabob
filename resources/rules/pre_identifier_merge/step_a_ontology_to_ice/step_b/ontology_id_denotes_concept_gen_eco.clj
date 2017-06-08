cdee493701b234f583814b418535460677f1a853
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-07 23:49:24 -0600

    Optimized rule bodies for faster query execution
    Via use of the Franz clauseReorderer=identity prefix

--- resources/rules/pre_identifier_merge/step_a_ontology_to_ice/step_b/ontology_id_denotes_concept_gen_eco.clj
+++ resources/rules/pre_identifier_merge/step_a_ontology_to_ice/step_b/ontology_id_denotes_concept_gen_eco.clj
@@ -5,20 +5,21 @@
 `{:name "ontology-id-denotes-concept-gen-eco"
   :description "This rule generates an ontology concept identifier for every non-root ontology concept with the EcoGene namespace."
   :head ((?/id obo/IAO_0000219 ?/ontology_concept) ; denotes
-         (?/id rdf/type ccp/IAO_EXT_0000088)) ; ontology concept identifier
+          (?/id rdf/type ccp/IAO_EXT_0000088)) ; ontology concept identifier
   :reify ([?/id {:ln (:regex "EcoGene:" "ECOGENE_" ?/concept_id)
                  :ns "ccp" :prefix "" :suffix ""}])
-  :sparql-string "prefix ccp: <http://ccp.ucdenver.edu/obo/ext/>
+  :sparql-string "prefix franzOption_clauseReorderer: <franz:identity>
+                  prefix ccp: <http://ccp.ucdenver.edu/obo/ext/>
                   prefix obo: <http://purl.obolibrary.org/obo/>
                   prefix oboInOwl: <http://www.geneontology.org/formats/oboInOwl#>
                   select ?ontology_concept ?concept_id {
-                  ?root_id rdf:type ccp:IAO_EXT_0000190 . # ccp:ontology root concept identifier
-                  ?root_id obo:IAO_0000219 ?root_class . # obo:denotes
-                  ?ontology_concept rdfs:subClassOf* ?root_class .
                   ?ontology_concept oboInOwl:id ?concept_id .
-                  minus {?ontology_concept owl:deprecated true} .
-                  minus {?ontology_concept rdf:type ccp:IAO_EXT_0000190} . # ccp:ontology root concept identifier
                   # include only concepts with the EcoGene namespace
                   filter (contains (str(?ontology_concept), 'http://www.ecogene.org/gene'))
-                  }"
+                  minus {?ontology_concept owl:deprecated true} .
+                  minus {?ontology_concept rdf:type ccp:IAO_EXT_0000190} . #ccp:ontology root concept identifier
+                  ?ontology_concept rdfs:subClassOf* ?root_class .
+                  ?root_id obo:IAO_0000219 ?root_class . #obo:denotes
+                  ?root_id rdf:type ccp:IAO_EXT_0000190 . #ccp:ontology root concept identifier
+  }"
   }
--- resources/rules/pre_identifier_merge/step_a_ontology_to_ice/step_b/ontology_id_denotes_concept_gen_pombase.clj
+++ resources/rules/pre_identifier_merge/step_a_ontology_to_ice/step_b/ontology_id_denotes_concept_gen_pombase.clj
@@ -8,17 +8,18 @@
          (?/id rdf/type ccp/IAO_EXT_0000088)) ; ontology concept identifier
   :reify ([?/id {:ln (:regex "PomBase:" "POMBASE_" ?/concept_id)
                  :ns "ccp" :prefix "" :suffix ""}])
-  :sparql-string "prefix ccp: <http://ccp.ucdenver.edu/obo/ext/>
+  :sparql-string "prefix franzOption_clauseReorderer: <franz:identity>
+                  prefix ccp: <http://ccp.ucdenver.edu/obo/ext/>
                   prefix obo: <http://purl.obolibrary.org/obo/>
                   prefix oboInOwl: <http://www.geneontology.org/formats/oboInOwl#>
                   select ?ontology_concept ?concept_id {
-                  ?root_id rdf:type ccp:IAO_EXT_0000190 . # ccp:ontology root concept identifier
-                  ?root_id obo:IAO_0000219 ?root_class . # obo:denotes
-                  ?ontology_concept rdfs:subClassOf* ?root_class .
                   ?ontology_concept oboInOwl:id ?concept_id .
-                  minus {?ontology_concept owl:deprecated true} .
-                  minus {?ontology_concept rdf:type ccp:IAO_EXT_0000190} . # ccp:ontology root concept identifier
                   # include only concepts with the PomBase namespace
                   filter (contains (str(?ontology_concept), 'http://www.pombase.org/spombe/result'))
-                  }"
+                  minus {?ontology_concept owl:deprecated true} .
+                  minus {?ontology_concept rdf:type ccp:IAO_EXT_0000190} . #ccp:ontology root concept identifier
+                  ?ontology_concept rdfs:subClassOf* ?root_class .
+                  ?root_id obo:IAO_0000219 ?root_class . #obo:denotes
+                  ?root_id rdf:type ccp:IAO_EXT_0000190 . #ccp:ontology root concept identifier
+  }"
   }
