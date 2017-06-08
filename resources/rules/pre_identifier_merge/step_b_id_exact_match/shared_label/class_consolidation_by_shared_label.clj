e2379850b0659b67724742c4a9f16639616ff1a3
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-08 13:41:45 -0600

    Revised query body for class_consolidation_by_shared_label rule
    The previous version regex’d each label to replace underscores with
    spaces. This operation is too costly to do for all labels vs. all
    labels. The current implementation is more efficient and executes in a
    reasonable amount of time.

--- resources/rules/pre_identifier_merge/step_b_id_exact_match/shared_label/class_consolidation_by_shared_label.clj
+++ resources/rules/pre_identifier_merge/step_b_id_exact_match/shared_label/class_consolidation_by_shared_label.clj
@@ -9,15 +9,14 @@
                   prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                   prefix oboInOwl: <http://www.geneontology.org/formats/oboInOwl#>
                   select ?id1 ?id2 {
-                             ?c1 oboInOwl:hasOBONamespace ?ns1 .
-                             ?c1 rdf:type owl:Class .
-                             ?c1 rdfs:label ?label1 .
-                             ?c2 oboInOwl:hasOBONamespace ?ns2 .
-                             ?c2 rdf:type owl:Class .
-                             ?c2 rdfs:label ?label2 .
-
-                             FILTER (?c1 != ?c2 && replace(str(?label1),'_',' ') = replace(str(?label2),'_',' ') && STR(IRI(?c1)) < STR(IRI(?c2)))
-                             ?id1 obo:IAO_0000219 ?c1 .
-                             ?id2 obo:IAO_0000219 ?c2 .
-                             }"
+                    ?c1 oboInOwl:hasOBONamespace ?ns1 .
+                    ?c1 rdf:type owl:Class .
+                    ?c1 rdfs:label ?label .
+                    ?c2 rdfs:label ?label .
+                    ?c2 oboInOwl:hasOBONamespace ?ns2 .
+                    ?c2 rdf:type owl:Class .
+                    FILTER (?c1 != ?c2 && STR(IRI(?c1)) < STR(IRI(?c2)))
+                    ?id1 obo:IAO_0000219 ?c1 .
+                    ?id2 obo:IAO_0000219 ?c2 .
+                  }"
   }
