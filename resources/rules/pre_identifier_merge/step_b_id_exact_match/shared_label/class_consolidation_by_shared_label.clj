0a0107b01d13b118504c863f5c8344a054749d59
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-08 11:47:08 -0600

    Initial commit of rule to link identifiers who denote classes that share a label

---
+++ resources/rules/pre_identifier_merge/step_b_id_exact_match/shared_label/class_consolidation_by_shared_label.clj
@@ -0,0 +1,23 @@
+;; ------------------------------------------------------------
+;; ---------  relation consolidation via shared label ---------
+;; ------------------------------------------------------------
+`{:name          "class-consolidation-by-shared_label"
+  :title         "creates exactMatch mappings between Class identifiers whose classes share the same exact label"
+  :head          ((?/id1 skos/exactMatch ?/id2))
+  :sparql-string "prefix franzOption_clauseReorderer: <franz:identity>
+                  prefix franzOption_chunkProcessingAllowed: <franz:yes>
+                  prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                  prefix oboInOwl: <http://www.geneontology.org/formats/oboInOwl#>
+                  select ?id1 ?id2 {
+                             ?c1 oboInOwl:hasOBONamespace ?ns1 .
+                             ?c1 rdf:type owl:Class .
+                             ?c1 rdfs:label ?label1 .
+                             ?c2 oboInOwl:hasOBONamespace ?ns2 .
+                             ?c2 rdf:type owl:Class .
+                             ?c2 rdfs:label ?label2 .
+
+                             FILTER (?c1 != ?c2 && replace(str(?label1),'_',' ') = replace(str(?label2),'_',' ') && STR(IRI(?c1)) < STR(IRI(?c2)))
+                             ?id1 obo:IAO_0000219 ?c1 .
+                             ?id2 obo:IAO_0000219 ?c2 .
+                             }"
+  }
