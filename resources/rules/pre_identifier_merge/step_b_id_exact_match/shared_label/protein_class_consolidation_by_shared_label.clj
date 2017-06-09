b5001ea91eea70b54f06cfa933fc642856a417a6
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-08 22:05:44 -0600

    Fixed query in protein_class_consolidation_by_shared_label rule

--- resources/rules/pre_identifier_merge/step_b_id_exact_match/shared_label/protein_class_consolidation_by_shared_label.clj
+++ resources/rules/pre_identifier_merge/step_b_id_exact_match/shared_label/protein_class_consolidation_by_shared_label.clj
@@ -5,10 +5,10 @@
   :title         "creates exactMatch mappings between Class identifiers whose classes have the label 'protein'. Generally we have excluded collapsing any PR concepts based on shared label for fear of accidentally collapsing genes with proteins. In the case of 'protein' however, we do want to collapse CHEBI:protein with PR:protein."
   :head          ((?/id1 skos/exactMatch ?/id2))
   :sparql-string "prefix franzOption_clauseReorderer: <franz:identity>
-                 prefix franzOption_chunkProcessingAllowed: <franz:yes>
-  prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    prefix oboInOwl: <http://www.geneontology.org/formats/oboInOwl#>
-select ?c1 ?c2 ?label {
+                  prefix franzOption_chunkProcessingAllowed: <franz:yes>
+                  prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                  prefix oboInOwl: <http://www.geneontology.org/formats/oboInOwl#>
+                  select ?id1 ?id2 {
                        ?c1 oboInOwl:hasOBONamespace ?ns1 .
                        ?c1 rdf:type owl:Class .
                        ?c1 rdfs:label ?label .
