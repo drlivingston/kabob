df9a435d2147333ddf051578cb715c9d17999eca
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-21 22:50:57 -0600

    Fixed ccp-extension-ontology URL after making the repo public

--- scripts/download/support-scripts_process-ontologies/download-ontologies.sh
+++ scripts/download/support-scripts_process-ontologies/download-ontologies.sh
@@ -35,7 +35,7 @@ http://purl.obolibrary.org/obo/uberon/ext.owl
 http://purl.obolibrary.org/obo/mi.owl
 http://purl.obolibrary.org/obo/hp.owl
 http://purl.obolibrary.org/obo/nbo.owl
-https://github.com/UCDenver-ccp/ccp-extension-ontology/releases/download/v0.1/ccp-extensions.owl
+https://raw.githubusercontent.com/UCDenver-ccp/ccp-extension-ontology/master/src/ontology/ccp-extensions.owl
 http://www.openannotation.org/spec/core/20130208/oa.owl
 "
 
