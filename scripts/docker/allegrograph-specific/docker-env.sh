703a3318d5b8fb1a68c1fa559fa22ab07ffc05fc
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-22 10:37:37 -0600

    The triple store container name is now accessed via a file placed in the load-request-directory

--- scripts/docker/allegrograph-specific/docker-env.sh
+++ scripts/docker/allegrograph-specific/docker-env.sh
@@ -4,11 +4,12 @@ export LOAD_REQUEST_DIRECTORY=/ag-load-requests
 
 # retrieve the AllegroGraph port being used from $LOAD_REQUEST_DIRECTORY/agraph.port
 export PLATFORM_KB_PORT=$(cat "${LOAD_REQUEST_DIRECTORY}/agraph.port")
+export CONTAINER_NAME=$(cat "${LOAD_REQUEST_DIRECTORY}/agraph.container.name")
 
 export PLATFORM_LEININGEN=/usr/local/bin/lein
 
 # Network URL refers to the container name
-export KB_INSTANCE_URL=http://agraph:${PLATFORM_KB_PORT}
+export KB_INSTANCE_URL=http://${CONTAINER_NAME}:${PLATFORM_KB_PORT}
 export KB_INSTANCE_USER=test
 export KB_INSTANCE_PASS=xyzzy
 export PLATFORM_DATASOURCE_OWL_DIR=/kabob_data/ontology
--- scripts/docker/virtuoso-specific/docker-env.sh
+++ scripts/docker/virtuoso-specific/docker-env.sh
@@ -4,11 +4,12 @@ export LOAD_REQUEST_DIRECTORY=/virtuoso-load-requests
 
 # retrieve the Virtuoso port being used from $LOAD_REQUEST_DIRECTORY/virtuoso.port
 export PLATFORM_KB_PORT=$(cat "${LOAD_REQUEST_DIRECTORY}/virtuoso.port")
+export CONTAINER_NAME=$(cat "${LOAD_REQUEST_DIRECTORY}/virtuoso.container.name")
 
 export PLATFORM_LEININGEN=/usr/local/bin/lein
 
 # Network URL refers to the container name
-export KB_INSTANCE_URL=http://virtuoso:${PLATFORM_KB_PORT}
+export KB_INSTANCE_URL=http://${CONTAINER_NAME}:${PLATFORM_KB_PORT}/sparql/
 export KB_INSTANCE_USER=dba
 export KB_INSTANCE_PASS=dba
 export PLATFORM_DATASOURCE_OWL_DIR=/kabob_data/ontology
