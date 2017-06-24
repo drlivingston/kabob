44295d1ce5f4e1bd2f8e481cd0176cb080a1d12f
callahantiff <callahantiff@gmail.com>
2017-06-24 19:56:00 -0400

    Fixed typo in uniprot tremble protein labels

--- resources/rules/post_identifier_merge/bio_labels/uniprot_swissprot_protein_labels.clj
+++ resources/rules/post_identifier_merge/bio_labels/uniprot_swissprot_protein_labels.clj
@@ -18,7 +18,7 @@
          (?/protein_symbol_field_value rdf/type ccp/IAO_EXT_0000932) ; ccp:UniProt_protein_record__name_field_value
          (?/protein_symbol_field_value rdfs/label ?/protein_symbol))
 
-   ;; In this case no `:reify` entry is required, since all of the data required to form the new triples already exist
+   ;; In this case no :reify entry is required, since all of the data required to form the new triples already exist
 
    :options {:magic-prefixes [["franzOption_chunkProcessingAllowed" "franz:yes"]]}
    }
