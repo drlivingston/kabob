e8f6488016fc8248d4c77e38dcbf9cead7d03a23
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-23 19:51:20 -0600

    Added rule and script to delete duplicate triples via SPARQL DELETE command

---
+++ resources/rules/utility/duplicates/delete_duplicate_triples.clj
@@ -0,0 +1,21 @@
+;; --------------------------------------------
+;; --------- Delete duplicate triples ---------
+;; --------------------------------------------
+`{:name "delete-duplicate-triples"
+  :description "This rule creates no new triples, but deletes all duplicate triples in the repository"
+  :head () ;; empty head
+  :reify ()
+  :sparql-string "prefix franzOption_chunkProcessingAllowed: <franz:yes>
+                  prefix franzOption_memoryLimit: <franz:85g>
+                  delete { graph ?g1 {?s ?p ?o}} where {
+                               select ?s ?p ?o  {
+                                   graph ?g1 {
+                                        ?s ?p ?o
+                                   }
+                                   graph ?g2 {
+                                        ?s ?p ?o
+                                   }
+                                   filter (?g1 != ?g2)
+                               }
+                  }"
+  }
