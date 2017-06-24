ad1e40cdbdb93ba5eb9bc88a21412252834fba6b
callahantiff <callahantiff@gmail.com>
2017-06-24 16:43:35 -0400

    Updated variables  from bp to biological process

--- resources/rules/post_identifier_merge/temp/goa/goa_biological_process_ice_to_bio.clj
+++ resources/rules/post_identifier_merge/temp/goa/goa_biological_process_ice_to_bio.clj
@@ -5,7 +5,7 @@
   :description "This rule creates a subclass of every biological process and types it as a gene ontology biological process concept identifier  (IAO_EXT_0000103)"
   :head (
          ;; create a subclass of the biological process
-         (?/process_sc rdfs/subClassOf ?/process)
+         (?/biological_process_sc rdfs/subClassOf ?/biological_process)
          ;; create a subclass of the participating bioentity
          (?/bioentity_sc rdfs/subClassOf ?/bioentity)
 
@@ -15,13 +15,13 @@
          (?/participation_restriction  owl/someValuesFrom ?/bioentity_sc)
 
          ;; connect the process subclass to the participation restriction
-         (?/process_sc rdfs/subClassOf ?/participation_restriction)
+         (?/biological_process_sc rdfs/subClassOf ?/participation_restriction)
 
          ;; provenance: connect the record to the process subclass
-         (?/record obo/IAO_0000219 ?/process_sc)) ; IAO:denotes
+         (?/record obo/IAO_0000219 ?/biological_process_sc)) ; IAO:denotes
     
   :body ((?/go_ice_id rdfs/subClassOf ccp/IAO_EXT_0000103) ; ccp:Gene_Ontology_Concept_Identifier__GO_biological_process_concept_identifier
-         (?/go_ice_id obo/IAO_0000219 ?/process) ; IAO:denotes
+         (?/go_ice_id obo/IAO_0000219 ?/biological_process) ; IAO:denotes
          (?/id_field_value rdf/type ?/go_ice_id)
          (?/id_field_value rdf/type ccp/IAO_EXT_0000014) ; ccp:GOA_GAF_v2.0_Annotation_record__ontology_term_identifier_field_value
          (?/record obo/BFO_0000051 ?/id_field_value) ; BFO:has_part
@@ -34,17 +34,16 @@
          (?/bioentity_ice_id obo/IAO_0000219 ?/participating_bioentity) ; IAO:denotes
 
          ;; filter out the negations
-          (:optional
-           ((?/record obo/BFO_0000051 ?/qualifier_field_value) ; BFO:has_part
-            (?/qualifier_field_value rdf/type ccp/IAO_EXT_0000013) ; ccp:GOA_GAF_v2.0_Annotation_record__qualifier_field_value
-            (?/qualifier_field_value rdfs/label ?/qualifier)))
+          (:optional ((?/record obo/BFO_0000051 ?/qualifier_field_value) ; BFO:has_part
+                      (?/qualifier_field_value rdf/type ccp/IAO_EXT_0000013) ; ccp:GOA_GAF_v2.0_Annotation_record__qualifier_field_value
+                      (?/qualifier_field_value rdfs/label ?/qualifier)))
           (:or (:not (:bound ?/qualifier))
                (:not (:regex ?/qualifier "^NOT" "i")))
          )
   
-  :reify ([?/process_sc {:ln (:sha-1 ?/process ?/participating_bioentity)
+  :reify ([?/biological_process_sc {:ln (:sha-1 ?/biological_process ?/participating_bioentity)
                  :ns "ccp" :prefix "BP_"}]
-          [?/bioentity_sc {:ln (:sha-1 ?/process ?/participating_bioentity)
+          [?/bioentity_sc {:ln (:sha-1 ?/biological_process ?/participating_bioentity)
                     :ns "ccp" :prefix "B_"}]
           [?/participation_restriction {:ln (:restriction)
                  :ns "ccp" :prefix "R_"}])
