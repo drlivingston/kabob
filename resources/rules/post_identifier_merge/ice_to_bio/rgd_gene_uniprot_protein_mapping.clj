53a97826a791226819b4b5c7f2c352ea7ac3938d
callahantiff <callahantiff@gmail.com>
2017-06-24 22:43:16 -0400

    Updated rgd gene uniprot protein coding rule to work with ccp ext ontology

---
+++ resources/rules/post_identifier_merge/ice_to_bio/rgd_gene_uniprot_protein_mapping.clj
@@ -0,0 +1,32 @@
+;; ----------------------------------------------------
+;; --------- RGD Gene Uniprot Protein Mapping ---------
+;; ----------------------------------------------------
+`{:name "rgd-gene-uniprot-protein-mapping"
+   :description "This rule maps rgd genes to uniprot proteins"
+   :head (
+          ;; map proteins to genes
+          (?/protein kso/has_indirect_template ?/gene)
+         
+          ;; create has indirect template restriction
+          (?/indirect_template_restriction rdf/type owl/Restriction)
+          (?/indirect_template_restriction owl/onProperty kso/has_indirect_template)
+          (?/indirect_template_restriction owl/someValuesFrom ?/gene)
+          (?/protein rdfs/subClassOf ?/indirect_template_restriction))
+
+    :body ((?/record rdf/type ccp/IAO_EXT_0000XXX) ; ccp:RGD_gene_record
+           (?/record obo/BFO_0000051 ?/field_1_field_value) ; BFO:has_part
+           (?/field_1_field_value rdf/type ccp/IAO_EXT_0000XXX) ; ccp:RGD_gene_record__UniProt_identifier_field_value
+           (?/field_1_field_value rdf/type ?/field_1_ice_id)
+           (?/field_1_ice_id obo/IAO_0000219 ?/protein) ; IAO:denotes
+
+           ;; retrieve the external exact match identifier
+           (?/record obo/BFO_0000051 ?/field_2_field_value) ; BFO:has_part
+           (?/field_2_field_value rdf/type ccp/IAO_EXT_0000XXX) ; ccp:RGD_gene_record__gene_identifier_field_value
+           (?/field_2_field_value rdf/type ?/field_2_ice_id)
+           (?/field_2_ice_id obo/IAO_0000219 ?/gene)) ; IAO:denotes
+
+    :reify ([?/indirect_template_restriction1 {:ln (:restriction)
+                   :ns "ccp" :prefix "R_"}])
+   
+    :options {:magic-prefixes [["franzOption_clauseReorderer" "franz:identity"]]}
+}
