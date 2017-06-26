2d5041c089570cced26f0cfd269a5c2add149ba5
callahantiff <callahantiff@gmail.com>
2017-06-26 14:31:26 -0400

    Corrected typo in rule

--- resources/rules/post_identifier_merge/temp/abstraction/gene_or_gene_product_abstraction.clj
+++ resources/rules/post_identifier_merge/temp/abstraction/gene_or_gene_product_abstraction.clj
@@ -1,11 +1,8 @@
 ;; ----------------------------------------------------
 ;; --------- Gene or Gene Product Abstraction ---------
 ;; ----------------------------------------------------
-;;TODO the restrictions need to be properly reified
-
 `{:name "gene-or-gene-product-abstraction"
   :description "This rule creates an absraction for referring to genes or gene products"
-  ;; this needs to be hashed and have proper classes
   :head ((?/gene_product rdfs/subClassOf ?/gene_or_gene_product))
   
   :body ((?/gene rdf/type kbio/GeneSpecificGClass)
@@ -14,7 +11,7 @@
 
          (?/has_indirect_template_restriction owl/someValuesFrom ?/gene)
          (?/has_indirect_template_restriction owl/onProperty obo/pr#has_gene_template) ; PR:has_gene_template
-         (?/gene_product rdfs/subClassOf?/has_indirect_template_restriction))
+         (?/gene_product rdfs/subClassOf? /has_indirect_template_restriction))
 
   :options {:magic-prefixes [["franzOption_clauseReorderer" "franz:identity"]]}
   }
