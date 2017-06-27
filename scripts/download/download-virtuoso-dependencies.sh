b6ca71ba334302b9b09ec89ba4d271c7cd82c3f9
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-27 16:15:28 -0600

    Added input arg to download-virtuoso-dependencies to specify maven binary

--- scripts/download/download-virtuoso-dependencies.sh
+++ scripts/download/download-virtuoso-dependencies.sh
@@ -5,18 +5,20 @@ if ! [[ -e README.md ]]; then
     exit 1
 fi
 
+MAVEN=$1
+
 mkdir -p mvn-local-repository
 
 wget -O ./mvn-local-repository/virt_rdf4j.jar \
      http://opldownload.s3.amazonaws.com/uda/virtuoso/7.2/rdfproviders/rdf4j/2/virt_rdf4j.jar
-mvn deploy:deploy-file -DgroupId=virtuoso -DartifactId=virtuoso-rdf4j \
+${MAVEN} deploy:deploy-file -DgroupId=virtuoso -DartifactId=virtuoso-rdf4j \
   -Dversion=2.x -Dpackaging=jar -Dfile=./mvn-local-repository/virt_rdf4j.jar \
   -Durl=file:mvn-local-repository
 rm ./mvn-local-repository/virt_rdf4j.jar
 
 wget -O ./mvn-local-repository/virtjdbc4_2.jar \
      http://opldownload.s3.amazonaws.com/uda/virtuoso/7.2/jdbc/virtjdbc4_2.jar
-mvn deploy:deploy-file -DgroupId=virtuoso -DartifactId=virtuoso-jdbc \
+${MAVEN} deploy:deploy-file -DgroupId=virtuoso -DartifactId=virtuoso-jdbc \
   -Dversion=4.2 -Dpackaging=jar -Dfile=./mvn-local-repository/virtjdbc4_2.jar \
   -Durl=file:mvn-local-repository
 rm ./mvn-local-repository/virtjdbc4_2.jar
