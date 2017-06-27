57ed177ecb31650eab734e1bf4b4296d3b04395f
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-27 16:55:21 -0600

    Added missing 'prefix'

--- resources/rules/pre_identifier_merge/pre_ice_rdf_load/step_a_ontology_to_ice/step_a_ontology_root_identifier_gen/root_property_identifier_gen.clj
+++ resources/rules/pre_identifier_merge/pre_ice_rdf_load/step_a_ontology_to_ice/step_a_ontology_root_identifier_gen/root_property_identifier_gen.clj
@@ -8,7 +8,7 @@
   :reify ([?/id {:ln (:localname ?/top_level_property)
                  :ns "ccp" :prefix "" :suffix ""}])
   :sparql-string "prefix oboInOwl: <http://www.geneontology.org/formats/oboInOwl#>
-                  rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                  prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                   prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
                   prefix owl: <http://www.w3.org/2002/07/owl#>
   select ?top_level_property {
