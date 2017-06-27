5fdb3e2d1d2a85cd93d7b028255c3bc428ead9f3
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-27 16:03:41 -0600

    Adding scripts to run rules against stardog in scripts/build

---
+++ scripts/build/run-rules-stardog.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPT_DIR/ENV.sh
+
+if [ $# -ne 7 ]
+then
+  echo "Usage: LEININGEN KB_URL KB_NAME KB_USER KB_PASS KB_DATA_DIR RULETYPE"
+  echo $@
+  exit 1
+fi
+
+LEININGEN=${1:?}
+KB_URL=${2:?}
+KB=${3:?}
+KB_USER=${4:?}
+KB_PASS=${5:?}
+KB_DATA_DIR=${6:?}
+RULETYPE=${7:?}
+
+OUTPUTDIR=$KB_DATA_DIR/$RULETYPE/
+RULES_RES_DIR=resources/$RULETYPE/
+
+# this flag is being used to indicate a stardog usage in this branch
+IS_VIRTUOSO=true
+
+echo Running rules
+mkdir -p ${OUTPUTDIR}
+$LEININGEN run-rules ${KB_URL} ${KB} ${KB_USER} ${KB_PASS} ${OUTPUTDIR} ${IS_VIRTUOSO} ${RULES_RES_DIR}
+
+# Ensure that all subdirectoes implicit in the `RULETYPE` are accessible to
+# later processes.
+find ${KB_DATA_DIR} -type d -exec chmod 0755 {} \;
