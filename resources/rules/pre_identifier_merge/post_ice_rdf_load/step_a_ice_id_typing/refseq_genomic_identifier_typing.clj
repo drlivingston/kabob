64c5042a1a04acaa64dc42aa7d23f86e6b1bf5ef
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-27 16:55:38 -0600

    Whitespace change only

--- resources/rules/pre_identifier_merge/post_ice_rdf_load/step_a_ice_id_typing/refseq_genomic_identifier_typing.clj
+++ resources/rules/pre_identifier_merge/post_ice_rdf_load/step_a_ice_id_typing/refseq_genomic_identifier_typing.clj
@@ -1,9 +1,9 @@
 ;; ----------------------------------------------------------
 ;; --------- refseq genomic identifier typing ---------------
 ;; ----------------------------------------------------------
-`{:name          "refseq-genomic-identifier-typing"
-  :description   "This rule specifically the basic formal ontology identifier"
-  :head          ((?/refseq_identifier rdf/type ccp/IAO_EXT_0001640)) ; CCP:refseq_genomic_identifier
+`{:name "refseq-genomic-identifier-typing"
+  :description "This rule specifically the basic formal ontology identifier"
+  :head ((?/refseq_identifier rdf/type ccp/IAO_EXT_0001640)) ; CCP:refseq_genomic_identifier
   :sparql-string "prefix obo: <http://purl.obolibrary.org/obo/>
                   prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                   prefix ccp: <http://ccp.ucdenver.edu/obo/ext/>
