84afcdb14f6401f661647bef9e6d04fca6395d48
callahantiff <callahantiff@gmail.com>
2017-06-26 22:26:26 -0400

    Fixed typo in triple

--- resources/rules/post_identifier_merge/temp/abstraction/gene_or_gene_product_abstraction.clj
+++ resources/rules/post_identifier_merge/temp/abstraction/gene_or_gene_product_abstraction.clj
@@ -11,7 +11,7 @@
 
          (?/has_indirect_template_restriction owl/someValuesFrom ?/gene)
          (?/has_indirect_template_restriction owl/onProperty obo/pr#has_gene_template) ; PR:has_gene_template
-         (?/gene_product rdfs/subClassOf? /has_indirect_template_restriction))
+         (?/gene_product rdfs/subClassOf ?/has_indirect_template_restriction))
 
   :options {:magic-prefixes [["franzOption_clauseReorderer" "franz:identity"]]}
   }
