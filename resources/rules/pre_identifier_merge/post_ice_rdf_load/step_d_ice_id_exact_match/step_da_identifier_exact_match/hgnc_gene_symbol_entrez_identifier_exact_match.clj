1cadabf40d44aa83cd7a16b40981bcb3902613c6
bill-baumgartner <bill.baumgartner@gmail.com>
2017-10-04 13:33:18 -0600

    Changes reflect update to the HGNC raw download

--- resources/rules/pre_identifier_merge/post_ice_rdf_load/step_d_ice_id_exact_match/step_da_identifier_exact_match/hgnc_gene_symbol_entrez_identifier_exact_match.clj
+++ resources/rules/pre_identifier_merge/post_ice_rdf_load/step_d_ice_id_exact_match/step_da_identifier_exact_match/hgnc_gene_symbol_entrez_identifier_exact_match.clj
@@ -9,14 +9,14 @@ PREFIX obo: <http://purl.obolibrary.org/obo/>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     SELECT ?hgnc_gene_symbol_identifier ?ncbi_gene_identifier
-WHERE {  ?record <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://ccp.ucdenver.edu/obo/ext/IAO_EXT_0000055> . # CCP:HGNC_gene_record
-?record <http://purl.obolibrary.org/obo/BFO_0000051> ?gene_symbol_field_value .
-?gene_symbol_field_value <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://ccp.ucdenver.edu/obo/ext/IAO_EXT_0000042> . # CCP:HGNC_gene_record_gene_symbol_field_value
-?gene_symbol_field_value <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?hgnc_gene_symbol_identifier .
-?hgnc_gene_symbol_identifier <http://www.w3.org/2000/01/rdf-schema#subClassOf> <http://ccp.ucdenver.edu/obo/ext/IAO_EXT_0000186> . # CCP:HGNC_gene_symbol_identifier
-?record <http://purl.obolibrary.org/obo/BFO_0000051> ?ncbi_gene_identifier_field_value .
-?ncbi_gene_identifier_field_value <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://ccp.ucdenver.edu/obo/ext/IAO_EXT_0000675> . # CCP:HGNC_gene_record_entrez_gene_identifier_field_value
-?ncbi_gene_identifier_field_value <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?ncbi_gene_identifier .
-?ncbi_gene_identifier <http://www.w3.org/2000/01/rdf-schema#subClassOf> <http://ccp.ucdenver.edu/obo/ext/IAO_EXT_0000084> . # ccp:NCBI_gene_identifier
+WHERE {  ?record rdf:type ccp:IAO_EXT_0000055 . # CCP:HGNC_gene_record
+?record obo:BFO_0000051 ?gene_symbol_field_value .
+?gene_symbol_field_value rdf:type ccp:IAO_EXT_0000042 . # CCP:HGNC_gene_record_gene_symbol_field_value
+?gene_symbol_field_value rdf:type ?hgnc_gene_symbol_identifier .
+?hgnc_gene_symbol_identifier rdfs:subClassOf ccp:IAO_EXT_0000186 . # CCP:HGNC_gene_symbol_identifier
+?record obo:BFO_0000051 ?ncbi_gene_identifier_field_value .
+?ncbi_gene_identifier_field_value rdf:type ccp:IAO_EXT_0000675 . # CCP:HGNC_gene_record_entrez_gene_identifier_field_value
+?ncbi_gene_identifier_field_value rdf:type ?ncbi_gene_identifier .
+?ncbi_gene_identifier rdfs:subClassOf ccp:IAO_EXT_0000084 . # ccp:NCBI_gene_identifier
 }"
 }
--- resources/rules/pre_identifier_merge/post_ice_rdf_load/step_d_ice_id_exact_match/step_da_identifier_exact_match/hgnc_gene_symbol_hgnc_identifier_exact_match.clj
+++ resources/rules/pre_identifier_merge/post_ice_rdf_load/step_d_ice_id_exact_match/step_da_identifier_exact_match/hgnc_gene_symbol_hgnc_identifier_exact_match.clj
@@ -9,14 +9,14 @@ PREFIX ccp: <http://ccp.ucdenver.edu/obo/ext/>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     SELECT ?hgnc_gene_symbol_identifier ?hgnc_gene_identifier
-WHERE {  ?record <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://ccp.ucdenver.edu/obo/ext/IAO_EXT_0000055> . # CCP:HGNC_gene_record
-?record <http://purl.obolibrary.org/obo/BFO_0000051> ?gene_symbol_field_value .
-?gene_symbol_field_value <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://ccp.ucdenver.edu/obo/ext/IAO_EXT_0000042> . # CCP:HGNC_gene_record_gene_symbol_field_value
-?gene_symbol_field_value <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?hgnc_gene_symbol_identifier .
-?hgnc_gene_symbol_identifier <http://www.w3.org/2000/01/rdf-schema#subClassOf> <http://ccp.ucdenver.edu/obo/ext/IAO_EXT_0000186> . # CCP:HGNC_gene_symbol_identifier
-?record <http://purl.obolibrary.org/obo/BFO_0000051> ?hgnc_gene_identifier_field_value .
-?hgnc_gene_identifier_field_value <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://ccp.ucdenver.edu/obo/ext/IAO_EXT_0000041> . # CCP:HGNC_gene_record_hgnc_identifier_field_value
-?hgnc_gene_identifier_field_value <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?hgnc_gene_identifier .
-?hgnc_gene_identifier <http://www.w3.org/2000/01/rdf-schema#subClassOf> <http://ccp.ucdenver.edu/obo/ext/IAO_EXT_0000185> . # CCP:HGNC_gene_identifier
+WHERE {  ?record rdf:type ccp:IAO_EXT_0000055 . # CCP:HGNC_gene_record
+?record obo:BFO_0000051 ?gene_symbol_field_value .
+?gene_symbol_field_value rdf:type ccp:IAO_EXT_0000042 . # CCP:HGNC_gene_record_gene_symbol_field_value
+?gene_symbol_field_value rdf:type ?hgnc_gene_symbol_identifier .
+?hgnc_gene_symbol_identifier rdfs:subClassOf ccp:IAO_EXT_0000186 . # CCP:HGNC_gene_symbol_identifier
+?record obo:BFO_0000051 ?hgnc_gene_identifier_field_value .
+?hgnc_gene_identifier_field_value rdf:type ccp:IAO_EXT_0000041 . # CCP:HGNC_gene_record_hgnc_identifier_field_value
+?hgnc_gene_identifier_field_value rdf:type ?hgnc_gene_identifier .
+?hgnc_gene_identifier rdfs:subClassOf ccp:IAO_EXT_0000185 . # CCP:HGNC_gene_identifier
 }"
 }
